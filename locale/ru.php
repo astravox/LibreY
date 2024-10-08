8837a6436c5f2668e32ac7f314fe9a2bb51c72f2
astravox <astravoxx@proton.me>
2024-10-08 20:33:24 +0200

    Create ru.php
    added russian version (im not a native speaker, might have some errors)

---
+++ locale/ru.php
@@ -0,0 +1,57 @@
+<?php
+
+return array(
+    "page_title" => "Поиск LibreY",
+    "search_button" => "Искать с LibreY",
+    "torrent_search_button" => "Искать торренты с LibreY",
+
+    "source_code_link" => "Исходный код",
+    "instances_link" => "Инстансы",
+    "settings_link" => "Настройки",
+    "api_link" => "API",
+    "donate_link" => "Пожертвовать ❤️",
+
+    "latest_commit" => "Последний коммит: %s",
+
+    "category_general" => "Общее",
+    "category_images" => "Изображения",
+    "category_videos" => "Видео",
+    "category_torrents" => "Торренты",
+    "category_tor" => "Tor",
+    "category_maps" => "Карты",
+
+    "feature_disabled" => "Хост отключил эту функцию :C",
+
+    "settings_title" => "Настройки",
+    "settings_theme" => "Тема",
+    "settings_special_warning" => "Этот флажок будет сохраняться, пока не будет нажата кнопка 'Сброс' или не будут очищены ваши файлы cookie.",
+    "settings_special_disabled" => "Отключить специальные запросы (например: конвертация валют)",
+
+    "settings_frontends" => "Конфиденциальные интерфейсы",
+    "settings_frontends_description" => "Например, если вы хотите смотреть YouTube без слежки, нажмите на \"Invidious\", найдите наиболее подходящий инстанс и вставьте его (правильный формат: https://example.com)",
+    "settings_frontends_disable" => "Отключить интерфейсы",
+
+    "settings_search_settings" => "Настройки поиска",
+    "settings_language" => "Язык",
+    "settings_preferred_engine" => "Предпочитаемый движок",
+
+    "settings_number_of_results" => "Количество результатов на странице",
+
+    "settings_safe_search" => "Безопасный поиск",
+    "settings_save" => "Сохранить",
+    "settings_reset" => "Сброс",
+
+
+    "failure_fallback" => "Результаты не найдены. Не удалось перейти на другие инстансы.",
+    "failure_empty" => "Результаты не найдены. Попробуйте другие ключевые слова!",
+    "result_no_description" => "Описание для этого сайта не предоставлено.",
+
+    "instances_librex" => "Следующие инстансы используют более старую версию %s",
+
+    "donate_original_developer" => "Пожертвовать оригинальному разработчику %s, проекту, который LibreY пытается улучшить.",
+    "donate_fork" => "Пожертвовать человеку, который форкнул %s в LibreY",
+
+    "api_unavailable" => "API LibreY недоступно в данный момент"
+);
+
+?>
